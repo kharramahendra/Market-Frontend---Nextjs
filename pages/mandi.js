b8ae9626220be3982af672bf8dda9eabf55536a1
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-02-03 21:53:36 +0530

    database change  commit

--- pages/mandi.js
+++ pages/mandi.js
@@ -156,13 +156,13 @@ const Mandi = ({ prices }) => {
 
                   {/* <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="" /> */}
                   <div className="flex-grow">
-                    <h2 className="text-gray-900 title-font font-medium">{price.timestamp}</h2>
-                    <div class="flex space-x-2 justify-center">
-                    <span class="text-xs inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-blue-600 text-white rounded">{price.keywords[0]}</span>
+                    <h2 className="text-green-900 title-font font-bold">{price.timestamp}</h2>
+                    <div class="flex justify-center">
+                    <span class="text-xs inline-block py-1 px-2.5 mr-2 leading-none text-center whitespace-nowrap align-baseline font-bold bg-blue-600 text-white rounded">{price.keywords[0]}</span>
                       {/* <span class="text-xs inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-purple-600 text-white rounded">Secondary</span> */}
-                      <span class="text-xs inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-green-500 text-white rounded">{price.keywords[1]}</span>
+                      <span class="text-xs inline-block py-1 px-2.5 mx-2 leading-none text-center whitespace-nowrap align-baseline font-bold bg-green-500 text-white rounded">{price.keywords[1]}</span>
                       {/* <span class="text-xs inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-red-600 text-white rounded">Danger</span> */}
-                      <span class="text-xs inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-yellow-500 text-white rounded">{price.keywords[2]}</span>
+                      <span class="text-xs inline-block py-1 px-2.5 ml-2 leading-none text-center whitespace-nowrap align-baseline font-bold bg-gray-800 text-white rounded">{price.keywords[2]}</span>
                       {/* <span class="text-xs inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-blue-400 text-white rounded">Info</span> */}
                       {/* <span class="text-xs inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-gray-200 text-gray-700 rounded">Light</span> */}
                       {/* <span class="text-xs inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-gray-800 text-white rounded">Dark</span> */}
