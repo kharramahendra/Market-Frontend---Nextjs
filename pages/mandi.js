0121c85b6fcb7dc3d9fcf8a94af6360153655079
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-02-03 21:28:15 +0530

    database change  commit

--- pages/mandi.js
+++ pages/mandi.js
@@ -6,7 +6,7 @@ import Link from 'next/link'
 import { useState } from 'react';
 let tvScriptLoadingPromise;
 
-const Mandi =({prices})=> {
+const Mandi = ({ prices }) => {
   const onLoadScriptRef = useRef();
 
 
@@ -69,8 +69,8 @@ const Mandi =({prices})=> {
 
   return (
     <>
-     
-     <Navbar />
+
+      <Navbar />
 
 
 
@@ -140,93 +140,29 @@ const Mandi =({prices})=> {
 
 
       <section className="text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-col text-center w-full mb-20">
+        <div className="container px-5 py-10 md:py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-8">
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Mandi Bhav</h1>
             <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Lates prices of commodities in various local mandis.</p>
           </div>
           <div className="flex flex-wrap -m-2">
-            <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/80x80" />
-                <div className="flex-grow">
-                  <h2 className="text-gray-900 title-font font-medium">2 Feb 2023</h2>
-                  <p className="text-gray-500">UI Designer</p>
+
+            {prices.map((price) => {
+              return <div key={price._id} className="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  {price.image != null && <img alt="bhav" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={process.env.NEXT_PUBLIC_HOST / post.image} />}
+                  {price.image == null && post.image_url != '' && <img className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={post.image_url} alt="tech" />}
+                  {price.image == null && post.image_url == '' && <img className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src='../assets/images/codes/temp4.jpg' alt="tech" />}
+
+                  {/* <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="" /> */}
+                  <div className="flex-grow">
+                    <h2 className="text-gray-900 title-font font-medium">{price.timestamp}</h2>
+                    <p className="text-gray-500">aaj ke taja bhav</p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/84x84" />
-                <div className="flex-grow">
-                  <h2 className="text-gray-900 title-font font-medium">1 Feb 2023</h2>
-                  <p className="text-gray-500">CTO</p>
-                </div>
-              </div>
-            </div>
-            <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/88x88" />
-                <div className="flex-grow">
-                  <h2 className="text-gray-900 title-font font-medium">31 Dec 2023</h2>
-                  <p className="text-gray-500">Founder</p>
-                </div>
-              </div>
-            </div>
-            <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/90x90" />
-                <div className="flex-grow">
-                  <h2 className="text-gray-900 title-font font-medium">John Doe</h2>
-                  <p className="text-gray-500">DevOps</p>
-                </div>
-              </div>
-            </div>
-            <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/94x94" />
-                <div className="flex-grow">
-                  <h2 className="text-gray-900 title-font font-medium">Martin Eden</h2>
-                  <p className="text-gray-500">Software Engineer</p>
-                </div>
-              </div>
-            </div>
-            <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/98x98" />
-                <div className="flex-grow">
-                  <h2 className="text-gray-900 title-font font-medium">Boris Kitua</h2>
-                  <p className="text-gray-500">UX Researcher</p>
-                </div>
-              </div>
-            </div>
-            <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/100x90" />
-                <div className="flex-grow">
-                  <h2 className="text-gray-900 title-font font-medium">Atticus Finch</h2>
-                  <p className="text-gray-500">QA Engineer</p>
-                </div>
-              </div>
-            </div>
-            <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/104x94" />
-                <div className="flex-grow">
-                  <h2 className="text-gray-900 title-font font-medium">Alper Kamu</h2>
-                  <p className="text-gray-500">System</p>
-                </div>
-              </div>
-            </div>
-            <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/108x98" />
-                <div className="flex-grow">
-                  <h2 className="text-gray-900 title-font font-medium">Rodrigo Monchi</h2>
-                  <p className="text-gray-500">Product Manager</p>
-                </div>
-              </div>
-            </div>
+            })}
+
           </div>
         </div>
       </section>
