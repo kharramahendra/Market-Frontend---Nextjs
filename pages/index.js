8b16c551cdd5d5260db5dac2db5bc2350cd02195
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-02-03 19:22:09 +0530

    database change  commit

--- pages/index.js
+++ pages/index.js
@@ -4,22 +4,11 @@ import Link from 'next/link'
 import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-
+import Navbar from '../components/navbar'
+import Footer from '../components/footer'
 
 const Home = ({ news,prices }) => {
-
-  const router = useRouter();
-  const [hidesearch, setHidesearch] = useState(true)
-  const [hidenotifications, setHidenav] = useState(true)
-  const [hidedropdown, setHidedrop] = useState(true)
-  const [query, setQuery] = useState('data science');
-
-  const handlechange = (e) => {
-    if (e.target.name == 'query') {
-      setQuery(e.target.value)
-    }
-  }
-
+  
   useEffect(() => {
 
     var js, fjs = document.getElementsByTagName('script')[0];
@@ -57,103 +46,10 @@ const Home = ({ news,prices }) => {
           
       </Head>
 
+      <Navbar />
 
 
-
-      {/* navbar */}
-      <nav className="relative px-4 py-2 bg-gray-900 flex justify-between items-center ">
-
-        <a className="text-3xl font-bold leading-none" href="#">
-          <img className="block h-10 w-auto " src="/logodark.png" alt="lets create idea" />
-        </a>
-
-
-
-        {/*  mobile search and hamburger menu  */}
-        <div className="lg:hidden flex">
-          <button onClick={() => setHidesearch(!hidesearch)} type="button" className="mx-2 flex text-gray-100 rounded-full lg:hidden text-sm items-center" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-            <span className="sr-only">View notifications</span>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#fff" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
-
-          </button>
-          <button onClick={() => setHidenav(!hidenotifications)} className="navbar-burger flex items-center text-gray-100 p-3">
-            <svg className="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <title>Mobile menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
-            </svg>
-          </button>
-        </div>
-        {/* end */}
-        <ul className="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6">
-          <li><Link href='/' legacyBehavior><a href='/' className="text-sm text-gray-100 hover:text-gray-200 font-bold">Home</a></Link></li>
-          <li><Link href='/sharemarket' legacyBehavior><a href="/sharemarket" className="text-sm text-gray-100 hover:text-gray-200 font-bold" >Share Market</a></Link></li>
-          <li><Link href={'/mandi'} legacyBehavior><a href="/mandi" className="text-sm text-gray-100 hover:text-gray-200 font-bold">Mandi Bhav</a></Link></li>
-          {/* <li><Link href={'/codes'}><a class="text-sm text-gray-100 hover:text-gray-200 font-bold" href="#">Codes</a></Link></li> */}
-          <li><Link href='/news' legacyBehavior><a href="/news" className="text-sm text-gray-100 hover:text-gray-200 font-bold" >News</a></Link></li>
-          <li><Link href={'/about'} legacyBehavior><a className="text-sm text-gray-100 flex hover:text-gray-200 font-bold">About</a></Link></li>
-          {/* <li><Link href={'/privacy.html'}><a class="text-sm text-gray-100 flex hover:text-gray-200 font-bold">Privacy policy</a></Link></li> */}
-        </ul>
-        <button onClick={() => setHidesearch(!hidesearch)} type="button" className="rounded-full lg:block hidden text-sm focus:outline-none " id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-          <span className="sr-only">View notifications</span>
-          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="#fff" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
-
-        </button>
-      </nav>
-
-      <div hidden={hidesearch} className="absolute right-0 z-50 mt-2 w-80 md:w-96 origin-bottom-right  rounded-lg bg-white  shadow-lg" role="menu" aria-orientation="vertical" >
-        <form>
-          <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only ">Search</label>
-          <div className="relative">
-            <input onChange={handlechange} type="search" value={query} name="query" id="query" className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 " placeholder="Search Codes, Projects..." required />
-            <Link href={`/search/${query}`}><button type="submit" className="text-gray-900 bg-white absolute right-2.5 bottom-2.5 border hover:bg-gray-900 hover:text-white border-gray-900  font-medium rounded-lg text-sm px-4 py-2 ">Search</button></Link>
-          </div>
-        </form>
-
-      </div>
-
-      <div className="navbar-menu absolute z-50" hidden={hidenotifications} >
-        <div className="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25"></div>
-        <nav className="fixed top-4 right-3 flex flex-col w-4/6 max-w-sm py-3 px-6 bg-white border-r rounded-lg  overflow-y-auto">
-          <div className="flex relative items-center mb-8">
-
-            <button className="absolute top-0 right-2 navbar-close" onClick={() => setHidenav(!hidenotifications)}>
-              <svg className="h-6 w-6 text-gray-900 cursor-pointer hover:text-gray-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
-              </svg>
-            </button>
-          </div>
-          <div>
-            <ul>
-              <li className="mb-1">
-                <Link href='/' legacyBehavior><a href='/' className="block p-2 text-sm font-semibold text-gray-800  rounded">Home</a></Link>
-              </li>
-
-              <li className="mb-1">
-                <Link href='/sharemarket' legacyBehavior><a href="/sharemarket" className="block p-2 text-sm font-semibold text-gray-800  rounded" >Share Market</a></Link>
-              </li>
-
-              <li className="mb-1">
-                <Link href={'/mandi'} legacyBehavior><a className="block p-2 text-sm font-semibold text-gray-800  rounded" >Mandi Bhav</a></Link>
-              </li>
-              <li className="mb-1">
-                <Link href='/news' legacyBehavior><a href='/news' className="block p-2 text-sm font-semibold text-gray-800  rounded" >News</a></Link>
-              </li>
-              <li className="mb-1 flex">
-                <Link href={'/about'} legacyBehavior><a className="flex p-2 text-sm font-semibold text-gray-800 rounded" >About</a></Link>
-              </li>
-
-
-            </ul>
-          </div>
-          <div className="mt-auto">
-
-            <p className="my-4 text-xs text-center text-gray-400">
-              <span>Copyright © 2023</span>
-            </p>
-          </div>
-        </nav>
-      </div>
-      {/* navbar end */}
+      
 
 
       {/* <Link href={'/p/market/sharemarket'}> */}
@@ -259,7 +155,7 @@ const Home = ({ news,prices }) => {
         <rssapp-imageboard id="toqVbAD3l5eo7qIF"></rssapp-imageboard><script src="https://widget.rss.app/v1/imageboard.js" type="text/javascript" async></script>
 
       </div>
-
+      <Footer />
 
     </>
   );
---
+++ pages/search/[slug].js
@@ -0,0 +1,99 @@
+import React from 'react'
+import Navbar from '../../components/navbar'
+import Footer from '../../components/footer'
+import { useState } from 'react';
+import Script from 'next/script'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+const Home = ({ posts }) => {
+
+  
+  return (<>
+   <Head>
+      <title>Tensorcodes.com - Search Results</title>
+      <meta name="description" content="find a collection of data science and machine learning codes,blogs and real-world projects that are designed to solve business problems." />
+      <link rel="icon" href="/favicon2.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+        <meta name="keywords" content="tensorcode,tensorcode.io,tensorcode io review,data science ,machine learning,deep learning, neural networks,AI" />
+        <meta property="og:title" content="Tensorcodes.com-find data science projects and codes" />
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content="https://images.unsplash.com/photo-1474314243412-cd4a79f02c6a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80" />
+        <meta property="og:image" content="https://images.unsplash.com/photo-1474314243412-cd4a79f02c6a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80" />
+        <meta charSet="utf-8"></meta>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8245105696445903"
+     crossorigin="anonymous"></script>
+    </Head>
+  <Navbar/>
+  <section class="pt-20 pb-10 lg:pt-[120px] lg:pb-20">
+        <div class="containerx">
+          <div class="-mx-4 flex flex-wrap justify-center">
+            <div class="w-full px-4">
+              <div class="mx-auto mb-[60px] max-w-[510px] text-center lg:mb-20">
+                <span class="mb-2 block text-lg font-semibold text-tensor">
+                  Search Results
+                </span>
+
+              </div>
+            </div>
+          </div>
+
+          <div class="-mx-4 flex flex-wrap">
+
+            {posts.map((post) => {
+              return <div ey={post._id} class="md:w-1/2 w-full mx-auto px-4">
+              <div class="w-full py-6">
+                <div class="flex w-full bg-white shadow-lg rounded-lg overflow-hidden">
+                  {post.image != null && <div class="w-1/3 bg-cover" style={{ backgroundImage: "url(" + process.env.NEXT_PUBLIC_HOST / post.image + ")", }}></div>}
+                  {post.image == null && post.image_url != '' && <div class="w-1/3 bg-cover" style={{ backgroundImage: "url(" + post.image_url + ")", }}></div>}
+                  {post.image == null && post.image_url == '' && <div class="w-1/3 bg-cover" style={{ backgroundImage: "url(" + '../assets/images/codes/temp4.jpg' + ")", }}></div>}
+      
+      
+                  <div class="w-2/3 p-4">
+                    <h1 class="text-rose-600 font-bold text-sm">{post.category}</h1>
+                    <p class=" text-gray-900 text-md font-semibold">{post.title}</p>
+                    <div class="flex item-center mt-2">
+                      <p class="text-base leading-relaxed text-gray-800">
+                        {post.timestamp}
+                      </p>
+      
+                    </div>
+                    <div class="flex item-center justify-between mt-3">
+                      <h1 class="text-gray-700 font-bold text-md">{post.keywords[0]}</h1>
+                      <Link href={`/post/${post.slug}`}><button class="px-3 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded">Learn More</button></Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            })}
+
+           
+
+          </div>
+        </div>
+      </section>
+<Footer/>
+  </>
+
+  )
+}
+
+
+export async function getServerSideProps(context) {
+  const data = { slug: context.query.slug }
+  let res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/search/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data),
+  })
+  const json_res = await res.json()
+  let posts = JSON.parse(JSON.stringify(json_res.posts))
+  console.log(posts)
+  return {
+    props: { posts: JSON.parse(JSON.stringify(posts)) }
+  }
+}
+export default Home
