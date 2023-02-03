541dd63ab3055983daf889ba4df3f1183300394b
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-02-03 21:38:03 +0530

    database change  commit

--- pages/mandi.js
+++ pages/mandi.js
@@ -148,9 +148,9 @@ const Mandi = ({ prices }) => {
           <div className="flex flex-wrap -m-2">
 
             {prices.map((price) => {
-              return <div key={price._id} className="p-2 lg:w-1/3 md:w-1/2 w-full">
+              return <Link key={price._id} href={`/post/${price.slug}`}><div  className="p-2 lg:w-1/3 md:w-1/2 w-full">
                 <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                  {price.image != null && <img alt="bhav" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={process.env.NEXT_PUBLIC_HOST / price.image} />}
+                  {price.image != null && <img alt="bhav" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-md mr-4" src={process.env.NEXT_PUBLIC_HOST / price.image} />}
                   {price.image == null && price.image_url != '' && <img className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={price.image_url} alt="tech" />}
                   {price.image == null && price.image_url == '' && <img className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src='../assets/images/codes/temp4.jpg' alt="tech" />}
 
@@ -160,7 +160,7 @@ const Mandi = ({ prices }) => {
                     <p className="text-gray-500">aaj ke taja bhav</p>
                   </div>
                 </div>
-              </div>
+              </div></Link>
             })}
 
           </div>
