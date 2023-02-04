e162773faf14db8c4d3fb5ca2d4fbefcbd35b1e1
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-02-04 15:48:49 +0530

    database change  commit

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -168,7 +168,7 @@ const Post = ({ post }) => {
                     <div class="w-full">
                         <div class="flex justify-items-end items-end">
                             <span
-                                class="my-2 inline-block rounded-lg bg-purple-100 py-1 px-3 text-sm font-semibold text-white"
+                                class="my-2 inline-block rounded-lg  py-1 px-3 text-sm font-semibold text-pink-700"
                             >
                                 {post.timestamp}
 
