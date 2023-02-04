4bffd90eb74e1cdc03b3ca01ac3e8200e5935db6
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-02-04 18:53:18 +0530

    database change  commit

--- pages/index.js
+++ pages/index.js
@@ -129,21 +129,21 @@ const Home = ({ news, prices }) => {
                   और सरकारी समाचार
                 </h1>
                 <div>
-                  <Link key={price._id} href={`/post/${price.slug}`}><div className=" cursor-pointer p-2 lg:w-1/3 md:w-1/2 w-full">
+                  <Link key={prices[0]._id} href={`/post/${prices[0].slug}`}><div className=" cursor-pointer p-2 lg:w-1/3 md:w-1/2 w-full">
                     <div className="h-full flex items-center bg-gray-200  border p-4 rounded-lg">
-                      {price.image != null && <img alt="bhav" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-md mr-4" src={process.env.NEXT_PUBLIC_HOST / price.image} />}
-                      {price.image == null && price.image_url != '' && <img className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-md mr-4" src={price.image_url} alt="tech" />}
-                      {price.image == null && price.image_url == '' && <img className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-md mr-4" src='../assets/images/codes/temp4.jpg' alt="tech" />}
+                      {prices[0].image != null && <img alt="bhav" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-md mr-4" src={process.env.NEXT_PUBLIC_HOST / prices[0].image} />}
+                      {prices[0].image == null && prices[0].image_url != '' && <img className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-md mr-4" src={prices[0].image_url} alt="tech" />}
+                      {prices[0].image == null && prices[0].image_url == '' && <img className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-md mr-4" src='../assets/images/codes/temp4.jpg' alt="tech" />}
 
                       {/* <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="" /> */}
                       <div className="flex-grow">
-                        <h2 className="text-green-900 title-font text-lg font-bold">{price.timestamp}</h2>
+                        <h2 className="text-green-900 title-font text-lg font-bold">{prices[0].timestamp}</h2>
                         <div class="flex">
-                          <span class="text-sm inline-block py-1 px-2.5 mr-2 leading-none text-center whitespace-nowrap align-baseline font-bold bg-purple-600 text-white rounded">{price.keywords[0]}</span>
+                          <span class="text-sm inline-block py-1 px-2.5 mr-2 leading-none text-center whitespace-nowrap align-baseline font-bold bg-purple-600 text-white rounded">{prices[0].keywords[0]}</span>
                           {/* <span class="text-xs inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-purple-600 text-white rounded">Secondary</span> */}
-                          <span class="text-sm inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-gray-200 text-gray-900 rounded">{price.keywords[1]}</span>
+                          <span class="text-sm inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-gray-200 text-gray-900 rounded">{prices[0].keywords[1]}</span>
                           {/* <span class="text-xs inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-red-600 text-white rounded">Danger</span> */}
-                          <span class="text-sm inline-block py-1 px-2.5 ml-2 leading-none text-center whitespace-nowrap align-baseline font-bold bg-gray-800 text-white rounded">{price.keywords[2]}</span>
+                          <span class="text-sm inline-block py-1 px-2.5 ml-2 leading-none text-center whitespace-nowrap align-baseline font-bold bg-gray-800 text-white rounded">{prices[0].keywords[2]}</span>
                           {/* <span class="text-xs inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-blue-400 text-white rounded">Info</span> */}
                           {/* <span class="text-xs inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-gray-200 text-gray-700 rounded">Light</span> */}
                           {/* <span class="text-xs inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-gray-800 text-white rounded">Dark</span> */}
