0376ed77e74901512e2518c93b9d4e7c85cdc05d
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-02-06 23:04:09 +0530

    covid 19

--- pages/about.js
+++ pages/about.js
@@ -42,7 +42,7 @@ export default function TradingViewWidget() {
 
             <section class="text-gray-600 body-font">
                 <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-                    <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="about" src="/assets/images/about.jpg" />
+                    <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="about" src="/assets/images/logodark2.jpg" />
                     <div class="text-center lg:w-2/3 w-full">
                         <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900"></h1>
                         <p class="mb-8 leading-relaxed">हमारी वेबसाइट KisanBazaar.in कृषि व्यवसाय के प्रेरकों और उत्पादकों के लिए एक पूर्ण स्थान है। हमारे प्लेटफार्म पर, हम सभी तरह के कृषि उत्पादों और फसलों के लाइव मूल्यों की जानकारी प्रदान करते हुए किसानों को समय-समय पर अपडेट करते हैं।
