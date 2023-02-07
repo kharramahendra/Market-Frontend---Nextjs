a4369f439a403a7d4d276ee3e25bab6b90f3ffe5
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-02-07 22:38:07 +0530

    covid 19

--- pages/sharemarket.js
+++ pages/sharemarket.js
@@ -39,99 +39,99 @@ export default function TradingViewWidget() {
             return () => onLoadScriptRef.current = null;
 
             function createWidget() {
-                if (document.getElementById('tradingview_afaa7') && 'TradingView' in window) {
-                    new window.TradingView.MediumWidget({
-                        symbols: [["JEERA", "NCDEX:JEERAJDR|3M"], ["GUAR", "NCDEX:GUARSEED10|3M"]],
-                        chartOnly: false,
-                        width: "100%",
-                        height: "100%",
-                        locale: "in",
-                        colorTheme: "light",
-                        autosize: true,
-                        showVolume: false,
-                        hideDateRanges: false,
-                        hideMarketStatus: false,
-                        hideSymbolLogo: false,
-                        scalePosition: "right",
-                        scaleMode: "Normal",
-                        fontFamily: "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
-                        fontSize: "10",
-                        noTimeScale: false,
-                        valuesTracking: "2",
-                        changeMode: "price-and-percent",
-                        chartType: "candlesticks",
-                        timeHoursFormat: "12-hours",
-                        upColor: "#22ab94",
-                        downColor: "#f7525f",
-                        borderUpColor: "#22ab94",
-                        borderDownColor: "#f7525f",
-                        wickUpColor: "#22ab94",
-                        wickDownColor: "#f7525f",
-                        container_id: "tradingview_afaa7"
-                    });
-                }
-                if (document.getElementById('tradingview_97945') && 'TradingView' in window) {
-                    new window.TradingView.MediumWidget({
-                        symbols: [["CHANA", "NCDEX:CHANA|3M"], ["GUAR", "NCDEX:GUARSEDBKN|3M"]],
-                        chartOnly: false,
-                        width: "100%",
-                        height: "100%",
-                        locale: "in",
-                        colorTheme: "light",
-                        autosize: true,
-                        showVolume: false,
-                        hideDateRanges: false,
-                        hideMarketStatus: false,
-                        hideSymbolLogo: false,
-                        scalePosition: "right",
-                        scaleMode: "Normal",
-                        fontFamily: "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
-                        fontSize: "10",
-                        noTimeScale: false,
-                        valuesTracking: "2",
-                        changeMode: "price-and-percent",
-                        chartType: "candlesticks",
-                        timeHoursFormat: "12-hours",
-                        upColor: "#22ab94",
-                        downColor: "#f7525f",
-                        borderUpColor: "#22ab94",
-                        borderDownColor: "#f7525f",
-                        wickUpColor: "#22ab94",
-                        wickDownColor: "#f7525f",
-                        container_id: "tradingview_97945"
-                    });
-                }
-                if (document.getElementById('tradingview_5e64a') && 'TradingView' in window) {
-                    new window.TradingView.MediumWidget({
-                        symbols: [["जौ ", "NCDEX:BARLEYJPR|3M"], ["BAJRA", "NCDEX:BAJRA|12M"], ["सरसों", "NCDEX:RMSEED|3M"]],
-                        chartOnly: false,
-                        width: "100%",
-                        height: "100%",
-                        locale: "in",
-                        colorTheme: "light",
-                        autosize: true,
-                        showVolume: false,
-                        hideDateRanges: false,
-                        hideMarketStatus: false,
-                        hideSymbolLogo: false,
-                        scalePosition: "right",
-                        scaleMode: "Normal",
-                        fontFamily: "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
-                        fontSize: "10",
-                        noTimeScale: false,
-                        valuesTracking: "2",
-                        changeMode: "price-and-percent",
-                        chartType: "candlesticks",
-                        timeHoursFormat: "12-hours",
-                        upColor: "#22ab94",
-                        downColor: "#f7525f",
-                        borderUpColor: "#22ab94",
-                        borderDownColor: "#f7525f",
-                        wickUpColor: "#22ab94",
-                        wickDownColor: "#f7525f",
-                        container_id: "tradingview_5e64a"
-                    });
-                }
+                // if (document.getElementById('tradingview_afaa7') && 'TradingView' in window) {
+                //     new window.TradingView.MediumWidget({
+                //         symbols: [["JEERA", "NCDEX:JEERAJDR|3M"], ["GUAR", "NCDEX:GUARSEED10|3M"]],
+                //         chartOnly: false,
+                //         width: "100%",
+                //         height: "100%",
+                //         locale: "in",
+                //         colorTheme: "light",
+                //         autosize: true,
+                //         showVolume: false,
+                //         hideDateRanges: false,
+                //         hideMarketStatus: false,
+                //         hideSymbolLogo: false,
+                //         scalePosition: "right",
+                //         scaleMode: "Normal",
+                //         fontFamily: "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
+                //         fontSize: "10",
+                //         noTimeScale: false,
+                //         valuesTracking: "2",
+                //         changeMode: "price-and-percent",
+                //         chartType: "candlesticks",
+                //         timeHoursFormat: "12-hours",
+                //         upColor: "#22ab94",
+                //         downColor: "#f7525f",
+                //         borderUpColor: "#22ab94",
+                //         borderDownColor: "#f7525f",
+                //         wickUpColor: "#22ab94",
+                //         wickDownColor: "#f7525f",
+                //         container_id: "tradingview_afaa7"
+                //     });
+                // }
+                // if (document.getElementById('tradingview_97945') && 'TradingView' in window) {
+                //     new window.TradingView.MediumWidget({
+                //         symbols: [["CHANA", "NCDEX:CHANA|3M"], ["GUAR", "NCDEX:GUARSEDBKN|3M"]],
+                //         chartOnly: false,
+                //         width: "100%",
+                //         height: "100%",
+                //         locale: "in",
+                //         colorTheme: "light",
+                //         autosize: true,
+                //         showVolume: false,
+                //         hideDateRanges: false,
+                //         hideMarketStatus: false,
+                //         hideSymbolLogo: false,
+                //         scalePosition: "right",
+                //         scaleMode: "Normal",
+                //         fontFamily: "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
+                //         fontSize: "10",
+                //         noTimeScale: false,
+                //         valuesTracking: "2",
+                //         changeMode: "price-and-percent",
+                //         chartType: "candlesticks",
+                //         timeHoursFormat: "12-hours",
+                //         upColor: "#22ab94",
+                //         downColor: "#f7525f",
+                //         borderUpColor: "#22ab94",
+                //         borderDownColor: "#f7525f",
+                //         wickUpColor: "#22ab94",
+                //         wickDownColor: "#f7525f",
+                //         container_id: "tradingview_97945"
+                //     });
+                // }
+                // if (document.getElementById('tradingview_5e64a') && 'TradingView' in window) {
+                //     new window.TradingView.MediumWidget({
+                //         symbols: [["जौ ", "NCDEX:BARLEYJPR|3M"], ["BAJRA", "NCDEX:BAJRA|12M"], ["सरसों", "NCDEX:RMSEED|3M"]],
+                //         chartOnly: false,
+                //         width: "100%",
+                //         height: "100%",
+                //         locale: "in",
+                //         colorTheme: "light",
+                //         autosize: true,
+                //         showVolume: false,
+                //         hideDateRanges: false,
+                //         hideMarketStatus: false,
+                //         hideSymbolLogo: false,
+                //         scalePosition: "right",
+                //         scaleMode: "Normal",
+                //         fontFamily: "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
+                //         fontSize: "10",
+                //         noTimeScale: false,
+                //         valuesTracking: "2",
+                //         changeMode: "price-and-percent",
+                //         chartType: "candlesticks",
+                //         timeHoursFormat: "12-hours",
+                //         upColor: "#22ab94",
+                //         downColor: "#f7525f",
+                //         borderUpColor: "#22ab94",
+                //         borderDownColor: "#f7525f",
+                //         wickUpColor: "#22ab94",
+                //         wickDownColor: "#f7525f",
+                //         container_id: "tradingview_5e64a"
+                //     });
+                // }
                 if (document.getElementById('tradingview_549eb') && 'TradingView' in window) {
                     new window.TradingView.MediumWidget({
                         symbols: [["जौ ", "NCDEX:BARLEYJPR|3M"], ["सरसों", "NCDEX:RMSEED|3M"], ["JEERA UNJHA", "NCDEX:JEERAUNJHA|3M"], ["JEERA JODHPUR", "NCDEX:JEERAJDR|3M"], [" गेहूं", "NCDEX:WHEATFAQ|3M"], ["कपास", "NCDEX:COTTON|3M"], ["BAJRA", "NCDEX:BAJRA|3M"], ["CHANA", "NCDEX:CHANA|3M"], ["MOONG", "NCDEX:MOONG|3M"], ["GUAR", "NCDEX:GUARSEED10|3M"]],
@@ -198,7 +198,7 @@ export default function TradingViewWidget() {
 
 
             {/* OVERALL */}
-            <div className="max-h-screen mb-20">
+            <div className="h-screen mb-20">
                 <div className='tradingview-widget-container'>
                     <div id='tradingview_549eb' />
                     <div className="tradingview-widget-copyright">
@@ -209,36 +209,36 @@ export default function TradingViewWidget() {
 
 
             {/* JODHPUR MANDI */}
-            <div className="h-screen my-20">
+            {/* <div className="h-screen my-20">
                 <div className='tradingview-widget-container'>
                     <div id='tradingview_afaa7' />
                     <div className="tradingview-widget-copyright">
                         <a rel="noopener" target="_blank"><span className="blue-text">JODHPUR MANDI</span></a>
                     </div>
                 </div>
-            </div>
+            </div> */}
 
 
 
             {/* BIKANER MANDI */}
-            <div className="h-screen my-20">
+            {/* <div className="h-screen my-20">
                 <div className='tradingview-widget-container'>
                     <div id='tradingview_97945' />
                     <div className="tradingview-widget-copyright">
                         <a rel="noopener" target="_blank"><span className="blue-text">Bikaner Mandi</span></a>
                     </div>
                 </div>
-            </div>
+            </div> */}
 
             {/* JAIPUR MANDI */}
-            <div className="h-screen my-20">
+            {/* <div className="h-screen my-20">
                 <div className='tradingview-widget-container'>
                     <div id='tradingview_5e64a' />
                     <div className="tradingview-widget-copyright">
                         <a rel="noopener" target="_blank"><span className="blue-text">JAIPUR MANDI</span></a>
                     </div>
                 </div>
-            </div>
+            </div> */}
 
 
 
