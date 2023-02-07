fafd5f49f7a07feeb2fc6a14cbe8f4fd35b58e33
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-02-07 22:35:01 +0530

    covid 19

--- pages/sharemarket.js
+++ pages/sharemarket.js
@@ -198,7 +198,7 @@ export default function TradingViewWidget() {
 
 
             {/* OVERALL */}
-            <div className="h-screen mb-20">
+            <div className="max-h-screen mb-20">
                 <div className='tradingview-widget-container'>
                     <div id='tradingview_549eb' />
                     <div className="tradingview-widget-copyright">
