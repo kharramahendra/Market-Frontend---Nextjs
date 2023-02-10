e689f03e9e57b6484e366322820722d3a999736c
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-02-10 16:55:46 +0530

    covid 19

--- pages/about.js
+++ pages/about.js
@@ -6,6 +6,12 @@ import { useState } from 'react';
 import Navbar from '../components/navbar'
 import Footer from '../components/footer'
 import Head from 'next/head'
+import { MdDateRange } from 'react-icons/md'
+import { BsFacebook, BsInstagram, BsGithub, BsLinkedin } from 'react-icons/bs'
+
+
+
+
 export default function TradingViewWidget() {
 
 
@@ -73,20 +79,17 @@ export default function TradingViewWidget() {
                                     <h3 class="text-gray-500 mb-3">Web Developer</h3>
                                     <p class="mb-4">Empowering the digital world, one line of code at a time.</p>
                                     <span class="inline-flex">
-                                        <a class="text-gray-500" href='https://www.facebook.com/kaluram.kharra'>
-                                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                                                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                                            </svg>
+                                        <a class="ml-2 text-gray-500" href='https://www.linkedin.com/in/kaluram-kharra/' target="_blank">
+                                            <BsLinkedin />
                                         </a>
-                                        <a class="ml-2 text-gray-500">
-                                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                                                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                                            </svg>
+                                        <a class="ml-2 text-gray-500" href='https://github.com/kaluramkharra' target="_blank">
+                                            <BsGithub />
                                         </a>
-                                        <a class="ml-2 text-gray-500">
-                                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                                                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                                            </svg>
+                                        <a class="text-gray-500" href='https://www.facebook.com/kaluram.kharra' target="_blank">
+                                            <BsFacebook />
+                                        </a>
+                                        <a class="ml-2 text-gray-500" href='https://www.instagram.com/kharra_kaluram/' target="_blank">
+                                            <BsInstagram />
                                         </a>
                                     </span>
                                 </div>
@@ -100,20 +103,17 @@ export default function TradingViewWidget() {
                                     <h3 class="text-gray-500 mb-3">Content creator</h3>
                                     <p class="mb-4">Crafting stories for the digital age, with passion and creativity.</p>
                                     <span class="inline-flex">
-                                        <a class="text-gray-500">
-                                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                                                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                                            </svg>
+                                        <a class="ml-2 text-gray-500" href='https://www.linkedin.com/in/kaluram-kharra/' target="_blank">
+                                            <BsLinkedin />
                                         </a>
-                                        <a class="ml-2 text-gray-500">
-                                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                                                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                                            </svg>
+                                        <a class="ml-2 text-gray-500" href='https://github.com/kaluramkharra' target="_blank">
+                                            <BsGithub />
                                         </a>
-                                        <a class="ml-2 text-gray-500">
-                                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                                                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                                            </svg>
+                                        <a class="text-gray-500" href='https://www.facebook.com/kaluram.kharra' target="_blank">
+                                            <BsFacebook />
+                                        </a>
+                                        <a class="ml-2 text-gray-500" href='https://www.instagram.com/kharra_kaluram/' target="_blank">
+                                            <BsInstagram />
                                         </a>
                                     </span>
                                 </div>
