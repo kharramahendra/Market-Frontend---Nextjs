4053a339ed24a750e737cfdcd535f8294ff4a1d2
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-02-10 16:57:14 +0530

    covid 19

--- pages/about.js
+++ pages/about.js
@@ -43,7 +43,7 @@ export default function TradingViewWidget() {
 
 
             <section class="text-gray-600 body-font">
-                <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+                <div class="container mx-auto flex px-5 pt-24 items-center justify-center flex-col">
                     <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="about" src="/logolight2.png" />
                     <div class="text-center lg:w-2/3 w-full">
                         <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900"></h1>
