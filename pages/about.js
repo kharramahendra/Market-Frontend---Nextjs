e8f13fd825da66ac6a63b78d6b3861546b1e03dd
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-02-10 20:43:57 +0530

    covid 19

--- pages/about.js
+++ pages/about.js
@@ -161,7 +161,7 @@ export default function TradingViewWidget() {
                         </div>
                         <div class="p-4 lg:w-1/2">
                             <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
-                                <img alt="team" class="flex-shrink-0 rounded-full w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="/assets/images/kharra.png"/>
+                                <img alt="team" class="flex-shrink-0 rounded-full w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="/assets/images/dhaka.jpg"/>
                                     <div class="flex-grow sm:pl-8">
                                         <h2 class="title-font font-medium text-lg text-gray-900">Alper Kamu</h2>
                                         <h3 class="text-gray-500 mb-3">Designer</h3>
