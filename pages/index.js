382c5ad7f43277fcca7d9100be56b79a17e6c2a8
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-02-25 17:41:16 +0530

    database commit

--- pages/index.js
+++ pages/index.js
@@ -157,7 +157,40 @@ const Home = ({ news, prices }) => {
           </div>
         </div>
 
-
+        <div class="tradingview-widget-container">
+          <div class="tradingview-widget-container__widget"></div>
+          <div class="tradingview-widget-copyright"><a href="https://in.tradingview.com/markets/" rel="noopener" target="_blank"><span class="blue-text">Markets today</span></a> by TradingView</div>
+          <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-tickers.js" async>
+            {JSON.stringify({
+              "symbols": [
+                {
+                  "proName": "FOREXCOM:SPXUSD",
+                  "title": "S&P 500"
+                },
+                {
+                  "proName": "FOREXCOM:NSXUSD",
+                  "title": "US 100"
+                },
+                {
+                  "proName": "FX_IDC:EURUSD",
+                  "title": "EUR/USD"
+                },
+                {
+                  "proName": "BITSTAMP:BTCUSD",
+                  "title": "Bitcoin"
+                },
+                {
+                  "proName": "BITSTAMP:ETHUSD",
+                  "title": "Ethereum"
+                }
+              ],
+              "colorTheme": "light",
+              "isTransparent": false,
+              "showSymbolLogo": true,
+              "locale": "in"
+            })}
+          </script>
+        </div>
 
 
         <a class="weatherwidget-io" href="https://forecast7.com/en/27d2073d75/nagaur/" data-label_1="NAGAUR" data-label_2="WEATHER" data-theme="pure" >NAGAUR WEATHER</a>
