a4e1d1610b1e624e04f69e962fe2dde4489a4dc8
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-03-09 19:04:48 +0530

    after adsense

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,77 @@ const Home = ({ news, prices }) => {
 
   useEffect(() => {
 
+    () => {
+      // onLoadScriptRef.current = createWidget;
+
+      if (!tvScriptLoadingPromise) {
+        tvScriptLoadingPromise = new Promise((resolve) => {
+          const script = document.createElement('script');
+          script.id = 'tradingview-widget-loading-script';
+          script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js';
+          script.type = 'text/javascript';
+          script.textContent = {
+            "symbols": [
+              {
+                "proName": "FOREXCOM:SPXUSD",
+                "title": "S&P 500"
+              },
+              {
+                "proName": "FOREXCOM:NSXUSD",
+                "title": "US 100"
+              },
+              {
+                "proName": "FX_IDC:EURUSD",
+                "title": "EUR/USD"
+              },
+              {
+                "proName": "BITSTAMP:BTCUSD",
+                "title": "Bitcoin"
+              },
+              {
+                "proName": "BITSTAMP:ETHUSD",
+                "title": "Ethereum"
+              }
+            ],
+            "showSymbolLogo": true,
+            "colorTheme": "light",
+            "isTransparent": false,
+            "displayMode": "adaptive",
+            "locale": "in"
+          }
+          script.onload = resolve;
+
+          document.head.appendChild(script);
+        });
+      }
+
+      tvScriptLoadingPromise.then(() => onLoadScriptRef.current && onLoadScriptRef.current());
+
+      return () => onLoadScriptRef.current = null;
+
+      // function createWidget() {
+      //   if (document.getElementById('tradingview_09e7c') && 'TradingView' in window) {
+      //     new window.TradingView.widget({
+      //       autosize: true,
+      //       symbol: "NCDEX:JEERAJDR",
+      //       timezone: "Etc/UTC",
+      //       theme: "light",
+      //       style: "1",
+      //       locale: "in",
+      //       toolbar_bg: "#f1f3f6",
+      //       enable_publishing: false,
+      //       range: "3M",
+      //       allow_symbol_change: true,
+      //       watchlist: ["NCDEX:JEERAJDR","NCDEX:RMSEED","NCDEX:GUARSEED10","NCDEX:GUARSEDBKN","NCDEX:WHEATFAQ","NCDEX:BARLEYJPR","NCDEX:BAJRA","NCDEX:MOONG","NCDEX:CHANA"],
+      //       details: true,
+      //       hotlist: true,
+      //       studies: ["ATR@tv-basicstudies","LinearRegression@tv-basicstudies","MASimple@tv-basicstudies","MAExp@tv-basicstudies"],
+      //       container_id: "tradingview_09e7c"
+      //     });
+      //   }
+      // }
+    }
+
     var js, fjs = document.getElementsByTagName('script')[0];
     if (!document.getElementById('weatherwidget-io-js')) {
       js = document.createElement('script');
@@ -61,7 +132,7 @@ const Home = ({ news, prices }) => {
           <h className="text-white text-lg"></h>
         </div> */}
 
-        {/* <div className="w-full z-10 overflow-x-hidden overflow-y-hidden h-20 lg:h-12">
+        <div className="w-full z-10 overflow-x-hidden overflow-y-hidden h-20 lg:h-12">
           <div className="-mr-10 overflow-hidden overflow-y-hidden overflow-x-hidden">
             <div className="tradingview-widget-container">
               <div className="tradingview-widget-container__widget">
@@ -69,7 +140,7 @@ const Home = ({ news, prices }) => {
               </div>
 
               <div className="tradingview-widget-copyright"></div>
-              <Script src='https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js'>
+              {/* <Script src='https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js'>
                 {JSON.stringify({
                   "symbols": [
                     {
@@ -111,11 +182,11 @@ const Home = ({ news, prices }) => {
                   "displayMode": "adaptive",
                   "locale": "in"
                 })}
-              </Script>
+              </Script> */}
               
             </div>
           </div>
-        </div> */}
+        </div>
 
 
 
--- pages/test.js
+++ pages/test.js
@@ -15,8 +15,37 @@ export default function TradingViewWidget() {
         tvScriptLoadingPromise = new Promise((resolve) => {
           const script = document.createElement('script');
           script.id = 'tradingview-widget-loading-script';
-          script.src = 'https://s3.tradingview.com/tv.js';
+          script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js';
           script.type = 'text/javascript';
+          script.textContent = {
+            "symbols": [
+              {
+                "proName": "FOREXCOM:SPXUSD",
+                "title": "S&P 500"
+              },
+              {
+                "proName": "FOREXCOM:NSXUSD",
+                "title": "US 100"
+              },
+              {
+                "proName": "FX_IDC:EURUSD",
+                "title": "EUR/USD"
+              },
+              {
+                "proName": "BITSTAMP:BTCUSD",
+                "title": "Bitcoin"
+              },
+              {
+                "proName": "BITSTAMP:ETHUSD",
+                "title": "Ethereum"
+              }
+            ],
+            "showSymbolLogo": true,
+            "colorTheme": "light",
+            "isTransparent": false,
+            "displayMode": "adaptive",
+            "locale": "in"
+          }
           script.onload = resolve;
 
           document.head.appendChild(script);
