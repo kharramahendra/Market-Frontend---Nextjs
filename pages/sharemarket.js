218b2ce230ddb28a475efe502be22838b1260c63
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-03-11 11:48:13 +0530

    database commit

--- pages/sharemarket.js
+++ pages/sharemarket.js
@@ -127,6 +127,37 @@ export default function TradingViewWidget() {
                 <meta charSet="utf-8"></meta>
                 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8245105696445903"
                     crossorigin="anonymous"></script>
+                     <Script type="text/javascript" strategy="beforeInteractive" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
+                    {JSON.stringify({
+                        "symbols": [
+                            {
+                                "proName": "FOREXCOM:SPXUSD",
+                                "title": "S&P 500"
+                            },
+                            {
+                                "proName": "FOREXCOM:NSXUSD",
+                                "title": "US 100"
+                            },
+                            {
+                                "proName": "FX_IDC:EURUSD",
+                                "title": "EUR/USD"
+                            },
+                            {
+                                "proName": "BITSTAMP:BTCUSD",
+                                "title": "Bitcoin"
+                            },
+                            {
+                                "proName": "BITSTAMP:ETHUSD",
+                                "title": "Ethereum"
+                            }
+                        ],
+                        "showSymbolLogo": true,
+                        "colorTheme": "light",
+                        "isTransparent": false,
+                        "displayMode": "adaptive",
+                        "locale": "in"
+                    })}
+                </Script>
             </Head>
             <body>
             <Navbar />
@@ -140,7 +171,7 @@ export default function TradingViewWidget() {
             <div class="tradingview-widget-container">
                 <div class="tradingview-widget-container__widget"></div>
                 <div class="tradingview-widget-copyright"><a href="https://in.tradingview.com/markets/" rel="noopener" target="_blank"><span class="blue-text">Markets today</span></a> by TradingView</div>
-                <Script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
+                <Script type="text/javascript" strategy="beforeInteractive" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
                     {JSON.stringify({
                         "symbols": [
                             {
