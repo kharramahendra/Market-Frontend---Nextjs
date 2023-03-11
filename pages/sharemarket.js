7e21feb2ce20b9c7216388e0308152ec44673ee2
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-03-11 13:44:14 +0530

    sharemarket views

--- pages/sharemarket.js
+++ pages/sharemarket.js
@@ -205,13 +205,13 @@ const ShareMarket = () =>{
                 <div class="tradingview-widget-container">
                     <div class="tradingview-widget-container__widget"></div>
                     <div class="tradingview-widget-copyright"><a href="https://in.tradingview.com/symbols/NCDEX-GUARSEED10/" rel="noopener" target="_blank"><span class="blue-text">GUARSEED10 quotes</span></a> by TradingView</div>
-                    <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-single-quote.js" async>
+                    <script src="https://s3.tradingview.com/external-embedding/embed-widget-single-quote.js" async>
                         
-                        {JSON.parse(`{"symbol": "NCDEX:GUARSEED10",
+                        {JSON.stringify({"symbol": "NCDEX:GUARSEED10",
                         "width": 350,
                         "colorTheme": "dark",
                         "isTransparent": false,
-                        "locale": "in"}`)}
+                        "locale": "in"})}
                         
                     </script>
                 </div>
