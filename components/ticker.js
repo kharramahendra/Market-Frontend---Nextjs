6fdca231033d99fae065c3e6e2b2bfc8f62e62ff
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-03-11 18:43:28 +0530

    sharemarket views

--- components/ticker.js
+++ components/ticker.js
@@ -70,36 +70,34 @@ const Ticker = () => {
             <div class="tradingview-widget-container__widget"></div>
             <div class="tradingview-widget-copyright"><a href="https://in.tradingview.com/markets/" rel="noopener" target="_blank"><span class="blue-text">Markets today</span></a> by TradingView</div>
             <script id="show-banner" src="https://s3.tradingview.com/external-embedding/embed-widget-tickers.js" async >
-                onload {() => {
-                    return {
-                        "symbols": [
-                            {
-                                "proName": "FOREXCOM:SPXUSD",
-                                "title": "S&P 500"
+                {
+                    "symbols": [
+                {
+                    "proName": "FOREXCOM:SPXUSD",
+                "title": "S&P 500"
                             },
-                            {
-                                "proName": "FOREXCOM:NSXUSD",
-                                "title": "US 100"
+                {
+                    "proName": "FOREXCOM:NSXUSD",
+                "title": "US 100"
                             },
-                            {
-                                "proName": "FX_IDC:EURUSD",
-                                "title": "EUR/USD"
+                {
+                    "proName": "FX_IDC:EURUSD",
+                "title": "EUR/USD"
                             },
-                            {
-                                "proName": "BITSTAMP:BTCUSD",
-                                "title": "Bitcoin"
+                {
+                    "proName": "BITSTAMP:BTCUSD",
+                "title": "Bitcoin"
                             },
-                            {
-                                "proName": "BITSTAMP:ETHUSD",
-                                "title": "Ethereum"
+                {
+                    "proName": "BITSTAMP:ETHUSD",
+                "title": "Ethereum"
                             }
-                        ],
-                        "colorTheme": "light",
-                        "isTransparent": false,
-                        "showSymbolLogo": true,
-                        "locale": "in"
+                ],
+                "colorTheme": "light",
+                "isTransparent": false,
+                "showSymbolLogo": true,
+                "locale": "in"
                     }
-                }}
             </script>
         </div>
     </>
