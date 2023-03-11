a98ad6ab128c36844ac2487c7cadf08b1426ec4e
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-03-11 17:54:47 +0530

    sharemarket views

--- components/ticker.js
+++ components/ticker.js
@@ -7,7 +7,7 @@ const Ticker = () => {
         <div class="tradingview-widget-container">
             <div class="tradingview-widget-container__widget"></div>
             <div class="tradingview-widget-copyright"><a href="https://in.tradingview.com/markets/" rel="noopener" target="_blank"><span class="blue-text">Markets today</span></a> by TradingView</div>
-            <Script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-tickers.js" async>
+            <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-tickers.js" async>
                 {JSON.stringify({
                     "symbols": [
                         {
@@ -36,7 +36,7 @@ const Ticker = () => {
                     "showSymbolLogo": true,
                     "locale": "in"
                 })}
-            </Script>
+            </script>
         </div>
     </>
 }
