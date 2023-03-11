0e54c324c7af976e81fed65dc3363b153e05c952
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-03-11 18:52:45 +0530

    sharemarket views

--- components/ticker.js
+++ components/ticker.js
@@ -4,72 +4,10 @@ import Script from 'next/script';
 const Ticker = () => {
 
     return <>
-
-        <Head>
-            {/* <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-tickers.js" async>
-
-                {`{"symbols": [
-                        {
-                            "proName": "FOREXCOM:SPXUSD",
-                            "title": "S&P 500"
-                        },
-                        {
-                            "proName": "FOREXCOM:NSXUSD",
-                            "title": "US 100"
-                        },
-                        {
-                            "proName": "FX_IDC:EURUSD",
-                            "title": "EUR/USD"
-                        },
-                        {
-                            "proName": "BITSTAMP:BTCUSD",
-                            "title": "Bitcoin"
-                        },
-                        {
-                            "proName": "BITSTAMP:ETHUSD",
-                            "title": "Ethereum"
-                        }
-                    ],
-                    "colorTheme": "light",
-                    "isTransparent": false,
-                    "showSymbolLogo": true,
-                    "locale": "in"
-}`}
-            </script>
-            <Script id="show-banner" strategy="lazyOnload" src="https://s3.tradingview.com/external-embedding/embed-widget-tickers.js">
-                {`{"symbols": [
-                        {
-                            "proName": "FOREXCOM:SPXUSD",
-                            "title": "S&P 500"
-                        },
-                        {
-                            "proName": "FOREXCOM:NSXUSD",
-                            "title": "US 100"
-                        },
-                        {
-                            "proName": "FX_IDC:EURUSD",
-                            "title": "EUR/USD"
-                        },
-                        {
-                            "proName": "BITSTAMP:BTCUSD",
-                            "title": "Bitcoin"
-                        },
-                        {
-                            "proName": "BITSTAMP:ETHUSD",
-                            "title": "Ethereum"
-                        }
-                    ],
-                    "colorTheme": "light",
-                    "isTransparent": false,
-                    "showSymbolLogo": true,
-                    "locale": "in"
-}`}
-            </Script> */}
-        </Head>
         <div class="tradingview-widget-container">
             <div class="tradingview-widget-container__widget"></div>
             <div class="tradingview-widget-copyright"><a href="https://in.tradingview.com/markets/" rel="noopener" target="_blank"><span class="blue-text">Markets today</span></a> by TradingView</div>
-            <Script id="show-banner" src="https://s3.tradingview.com/external-embedding/embed-widget-tickers.js" async >
+            <Script id="show-banner" src="https://s3.tradingview.com/external-embedding/embed-widget-tickers.js" async 
                 dangerouslySetInnerHTML={{
                     __html: `{
                     "symbols": [
@@ -100,7 +38,7 @@ const Ticker = () => {
                     "locale": "in"}`,
                 }}
                 
-            </Script>
+           />
         </div>
     </>
 }
