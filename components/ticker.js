865d65a17cdf652b8732dd8a5a914c33d4829a09
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-03-11 18:23:48 +0530

    sharemarket views

--- components/ticker.js
+++ components/ticker.js
@@ -6,7 +6,7 @@ const Ticker = () => {
     return <>
 
         <Head>
-            <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-tickers.js" async>
+            {/* <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-tickers.js" async>
 
                 {`{"symbols": [
                         {
@@ -36,45 +36,68 @@ const Ticker = () => {
                     "locale": "in"
 }`}
             </script>
-        </Head>
-        <div class="tradingview-widget-container">
-            <div class="tradingview-widget-container__widget"></div>
-            <div class="tradingview-widget-copyright"><a href="https://in.tradingview.com/markets/" rel="noopener" target="_blank"><span class="blue-text">Markets today</span></a> by TradingView</div>
-            <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-tickers.js" async>
-
-                dangerouslySetInnerHTML={{
-                    __html: `{
-                        "symbols": [
-                    {
-                        "proName": "FOREXCOM:SPXUSD",
-                    "title": "S&P 500"
-                            },
-                    {
-                        "proName": "FOREXCOM:NSXUSD",
-                    "title": "US 100"
-                            },
-                    {
-                        "proName": "FX_IDC:EURUSD",
-                    "title": "EUR/USD"
-                            },
-                    {
-                        "proName": "BITSTAMP:BTCUSD",
-                    "title": "Bitcoin"
-                            },
-                    {
-                        "proName": "BITSTAMP:ETHUSD",
-                    "title": "kalu"
-                            }
+            <Script id="show-banner" strategy="lazyOnload" src="https://s3.tradingview.com/external-embedding/embed-widget-tickers.js">
+                {`{"symbols": [
+                        {
+                            "proName": "FOREXCOM:SPXUSD",
+                            "title": "S&P 500"
+                        },
+                        {
+                            "proName": "FOREXCOM:NSXUSD",
+                            "title": "US 100"
+                        },
+                        {
+                            "proName": "FX_IDC:EURUSD",
+                            "title": "EUR/USD"
+                        },
+                        {
+                            "proName": "BITSTAMP:BTCUSD",
+                            "title": "Bitcoin"
+                        },
+                        {
+                            "proName": "BITSTAMP:ETHUSD",
+                            "title": "Ethereum"
+                        }
                     ],
                     "colorTheme": "light",
                     "isTransparent": false,
                     "showSymbolLogo": true,
                     "locale": "in"
-                    }`,
-                }}
-
-
-            </script>
+}`}
+            </Script> */}
+        </Head>
+        <div class="tradingview-widget-container">
+            <div class="tradingview-widget-container__widget"></div>
+            <div class="tradingview-widget-copyright"><a href="https://in.tradingview.com/markets/" rel="noopener" target="_blank"><span class="blue-text">Markets today</span></a> by TradingView</div>
+            <Script id="show-banner" strategy="lazyOnload" src="https://s3.tradingview.com/external-embedding/embed-widget-tickers.js">
+                {`{"symbols": [
+                        {
+                            "proName": "FOREXCOM:SPXUSD",
+                            "title": "S&P 500"
+                        },
+                        {
+                            "proName": "FOREXCOM:NSXUSD",
+                            "title": "US 100"
+                        },
+                        {
+                            "proName": "FX_IDC:EURUSD",
+                            "title": "EUR/USD"
+                        },
+                        {
+                            "proName": "BITSTAMP:BTCUSD",
+                            "title": "Bitcoin"
+                        },
+                        {
+                            "proName": "BITSTAMP:ETHUSD",
+                            "title": "Ethereum"
+                        }
+                    ],
+                    "colorTheme": "light",
+                    "isTransparent": false,
+                    "showSymbolLogo": true,
+                    "locale": "in"
+}`}
+            </Script>
         </div>
     </>
 }
