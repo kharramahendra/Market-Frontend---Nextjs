3568255ddfb368f4333b8d7ab39c49b429fe3d5d
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-03-11 14:03:22 +0530

    sharemarket views

--- pages/sharemarket.js
+++ pages/sharemarket.js
@@ -154,53 +154,7 @@ const ShareMarket = () =>{
                 <Navbar />
 
 
-                <div class="tradingview-widget-container top-1 right-1 absolute" >
-                    <div class="tradingview-widget-container__widget" ></div>
-                    {/* <div class="tradingview-widget-copyright"></div> */}
-                    <Script type="text/javascript"  strategy="beforeInteractive" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
-                       {JSON.stringify({
-                            "symbols" : [
-                                {
-                                    "description": "GUAR ",
-                                    "proName": "NCDEX:GUARSEED10"
-                                },
-                                {
-                                    "description": "JEERA",
-                                    "proName": "NCDEX:JEERAJDR"
-                                },
-                                {
-                                    "description": "जौ",
-                                    "proName": "NCDEX:BARLEYJPR"
-                                },
-                                {
-                                    "description": "गेहूं",
-                                    "proName": "NCDEX:WHEATFAQ"
-                                },
-                                {
-                                    "description": "सरसों",
-                                    "proName": "NCDEX:RMSEED"
-                                },
-                                {
-                                    "description": "MOONG",
-                                    "proName": "NCDEX:MOONG"
-                                },
-                                {
-                                    "description": "CHANA",
-                                    "proName": "NCDEX:CHANA"
-                                },
-                                {
-                                    "description": "BAJRA",
-                                    "proName": "NCDEX:BAJRA"
-                                }],
-                            "showSymbolLogo": true,
-                            "colorTheme":"light",
-                            "isTransparent": false,
-                            "displayMode":"adaptive",
-                            "locale":"in"
-                        })}
-
-                    </Script>
-                </div>
+            
 
 
 
