8ae9f1889c1ddbf39e1403e2d0db5452c9ca17d1
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-03-11 12:19:46 +0530

    database commit

--- pages/sharemarket.js
+++ pages/sharemarket.js
@@ -155,7 +155,7 @@ export default function TradingViewWidget() {
                 <div class="tradingview-widget-container">
                     <div class="tradingview-widget-container__widget"></div>
                     <div class="tradingview-widget-copyright"><a href="https://in.tradingview.com/markets/" rel="noopener" target="_blank"><span class="blue-text">Markets today</span></a> by TradingView</div>
-                    <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
+                    <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" >
                         {/* {JSON.stringify({
                             "symbols": [
                                 {
