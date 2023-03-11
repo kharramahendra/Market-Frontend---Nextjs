2f15bd713e4a3db0baf0f4e63d112f40b1f1d625
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-03-11 11:34:53 +0530

    after adsense

--- pages/sharemarket.js
+++ pages/sharemarket.js
@@ -130,6 +130,23 @@ export default function TradingViewWidget() {
             <Navbar />
 
 
+
+            <div class="tradingview-widget-container">
+                <div class="tradingview-widget-container__widget"></div>
+                <div class="tradingview-widget-copyright"><a href="https://in.tradingview.com/symbols/NCDEX-GUARSEED10/" rel="noopener" target="_blank"><span class="blue-text">GUARSEED10 quotes</span></a> by TradingView</div>
+                <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-single-quote.js" async>
+                    {JSON.stringify({
+                        "symbol": "NCDEX:GUARSEED10",
+                    "width": "100%",
+                    "colorTheme": "dark",
+                    "isTransparent": false,
+                    "locale": "in"
+})}
+                </script>
+            </div>
+
+
+
             <div class="tradingview-widget-container">
                 <div class="tradingview-widget-container__widget"></div>
                 <div class="tradingview-widget-copyright"><a href="https://in.tradingview.com/markets/" rel="noopener" target="_blank"><span class="blue-text">Markets today</span></a> by TradingView</div>
