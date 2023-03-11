03fce43581e0513f7473b9984fa4b84ec4d22c71
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-03-11 18:10:23 +0530

    sharemarket views

--- components/ticker.js
+++ components/ticker.js
@@ -41,6 +41,38 @@ const Ticker = () => {
             <div class="tradingview-widget-container__widget"></div>
             <div class="tradingview-widget-copyright"><a href="https://in.tradingview.com/markets/" rel="noopener" target="_blank"><span class="blue-text">Markets today</span></a> by TradingView</div>
             <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-tickers.js" async>
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `{
+                        "symbols": [
+                    {
+                        "proName": "FOREXCOM:SPXUSD",
+                    "title": "S&P 500"
+                            },
+                    {
+                        "proName": "FOREXCOM:NSXUSD",
+                    "title": "US 100"
+                            },
+                    {
+                        "proName": "FX_IDC:EURUSD",
+                    "title": "EUR/USD"
+                            },
+                    {
+                        "proName": "BITSTAMP:BTCUSD",
+                    "title": "Bitcoin"
+                            },
+                    {
+                        "proName": "BITSTAMP:ETHUSD",
+                    "title": "Ethereum"
+                            }
+                    ],
+                    "colorTheme": "light",
+                    "isTransparent": false,
+                    "showSymbolLogo": true,
+                    "locale": "in"
+                    }`,
+                }}
+
                 {/* {`{
                     "symbols": [
                 {
