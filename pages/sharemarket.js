c2d6b7bb8d27731adeac23795e8a3cd248945f3b
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-03-11 12:29:57 +0530

    sharemarket views

--- pages/sharemarket.js
+++ pages/sharemarket.js
@@ -1,5 +1,5 @@
 // TradingViewWidget.js
-import createDOMPurify from 'dompurify'
+// import createDOMPurify from 'dompurify'
 import React, { useEffect, useRef } from 'react';
 import Link from 'next/link'
 import { useState } from 'react';
@@ -15,7 +15,47 @@ export default function TradingViewWidget() {
     const [hidenotifications, setHidenav] = useState(true)
     const [hidedropdown, setHidedrop] = useState(true)
     const [query, setQuery] = useState('data science');
-
+    const obj = {
+        "symbols": [
+            {
+                "description": "GUAR ",
+                "proName": "NCDEX:GUARSEED10"
+            },
+            {
+                "description": "JEERA",
+                "proName": "NCDEX:JEERAJDR"
+            },
+            {
+                "description": "जौ",
+                "proName": "NCDEX:BARLEYJPR"
+            },
+            {
+                "description": "गेहूं",
+                "proName": "NCDEX:WHEATFAQ"
+            },
+            {
+                "description": "सरसों",
+                "proName": "NCDEX:RMSEED"
+            },
+            {
+                "description": "MOONG",
+                "proName": "NCDEX:MOONG"
+            },
+            {
+                "description": "CHANA",
+                "proName": "NCDEX:CHANA"
+            },
+            {
+                "description": "BAJRA",
+                "proName": "NCDEX:BAJRA"
+            }
+        ],
+        "showSymbolLogo": true,
+        "colorTheme": "light",
+        "isTransparent": false,
+        "displayMode": "adaptive",
+        "locale": "in"
+    }
     
 
     const handlechange = (e) => {
@@ -116,7 +156,7 @@ export default function TradingViewWidget() {
                     <div class="tradingview-widget-container__widget"></div>
                     {/* <div class="tradingview-widget-copyright"></div> */}
                     <Script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" >
-                        {JSON.stringify({
+                        {/* {JSON.stringify({
                             "symbols": [
                                 {
                                     "description": "GUAR ",
@@ -156,8 +196,8 @@ export default function TradingViewWidget() {
                             "isTransparent": false,
                             "displayMode": "adaptive",
                             "locale": "in"
-                        })}
-                     
+                        })} */}
+                     obj
                     </Script>
                 </div>
 
