b44d1221e8515c59c580ee81589b8f77ecbaf7e1
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-03-16 21:22:54 +0530

    sharemarket views

--- components/footer.js
+++ components/footer.js
@@ -9,33 +9,7 @@ const Footer = ({ news }) => {
     return (
         <>
 
-            <section class="text-gray-600 body-font">
-                <div class="containerx px-5 py-24 mx-auto">
-                    <div class="flex flex-wrap -mx-4 -mb-10 text-center">
-
-                        {news.map((post) => {
-                            return <div key={post._id} class="mb-7 mx-2 bg-gray-100  sm:mx-auto w-full md:w-2/5 rounded shadow-lg flex card text-grey-darkest">
-                                {post.image != null && <img class="w-1/2 h-full object-cover object-center rounded-l-lg" src={process.env.NEXT_PUBLIC_HOST + post.image} alt="tech" />}
-                                {post.image == null && post.image_url != '' && <img class="lw-1/2 h-full object-cover object-center rounded-l-lg" src={post.image_url} alt="tech" />}
-                                {post.image == null && post.image_url == '' && <img class="w-1/2 h-full object-cover object-center rounded-l-lg" src='../assets/images/news.jpg' alt="tech" />}
-                                <div class="w-1/2 flex flex-col">
-                                    <div class="p-2 pb-0 flex-col">
-
-                                        <span class="text-md md:text-xl font-semibold md:font-bold text-black">{post.title.slice(0, 70)} ...</span>
-
-                                        <Link href={`/blog/${post.slug}`}><button class="px-2 md:px-3 md:py-2 ml-2 my-1 py-1 hover:shadow-md bg-gray-800 text-white text-xs font-bold uppercase rounded leading-tight hover:bg-gray-900 ">और अधिक जानें</button></Link>
-
-                                    </div>
-
-                                </div>
-                            </div>
-                        })}
-
-
-
-                    </div>
-                </div>
-            </section>
+          
 
 
 
@@ -84,15 +58,5 @@ const Footer = ({ news }) => {
         </>
     )
 }
-export async function getServerSideProps(context) {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/home/`)
-    const json_res = await res.json()
-    let posts = JSON.parse(JSON.stringify(json_res))
-    let news = posts.news
-    let prices = posts.prices
-    console.log(prices)
-    return {
-        props: { news: JSON.parse(JSON.stringify(news)), prices: JSON.parse(JSON.stringify(prices)) }
-    }
-}
+
 export default Footer
