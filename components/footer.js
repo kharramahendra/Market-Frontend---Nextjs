1f26ca3b9e30fb067323ebb9f695b101acc60258
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-03-16 21:32:16 +0530

    sharemarket views

--- components/footer.js
+++ components/footer.js
@@ -5,12 +5,114 @@ import { useRouter } from 'next/dist/client/router'
 import { AiFillGithub, AiFillInstagram } from 'react-icons/ai';
 const Footer = ({ news }) => {
 
-
     return (
         <>
 
-          
 
+            <section class="text-gray-600 body-font">
+                <div class="containerx px-5 py-24 mx-auto">
+                    <div class="flex flex-wrap -mx-4 -mb-10 text-center">
+
+                        {news.map((post) => {
+                            return <div key={post._id} class="mb-7 mx-2 bg-gray-100  sm:mx-auto w-full md:w-2/5 rounded shadow-lg flex card text-grey-darkest">
+                                {post.image != null && <img class="w-1/2 h-full object-cover object-center rounded-l-lg" src={process.env.NEXT_PUBLIC_HOST + post.image} alt="tech" />}
+                                {post.image == null && post.image_url != '' && <img class="lw-1/2 h-full object-cover object-center rounded-l-lg" src={post.image_url} alt="tech" />}
+                                {post.image == null && post.image_url == '' && <img class="w-1/2 h-full object-cover object-center rounded-l-lg" src='../assets/images/news.jpg' alt="tech" />}
+                                {/* <img class="w-1/2 h-full rounded-l-sm" src="https://bit.ly/2EApSiC" alt="Room Image" /> */}
+                                <div class="w-1/2 flex flex-col">
+                                    <div class="p-2 pb-0 flex-col">
+
+                                        <span class="text-md md:text-xl font-semibold md:font-bold text-black">{post.title.slice(0, 70)} ...</span>
+                                        {/* <div class="flex items-center mt-4">
+                      <div class="pr-2 text-xs">
+                        <i class="fas fa-wifi text-green"></i> Free WiFi
+                      </div>
+                      <div class="px-2 text-xs">
+                        <i class="text-grey-darker far fa-building"></i> 2mins to center
+                      </div>
+                    </div> */}
+                                        <Link href={`/blog/${post.slug}`}><button class="px-2 md:px-3 md:py-2 ml-2 my-1 py-1 hover:shadow-md bg-gray-800 text-white text-xs font-bold uppercase rounded leading-tight hover:bg-gray-900 ">और अधिक जानें</button></Link>
+
+                                    </div>
+                                    {/* <div
+                    class="bg-grey-lighter p-3 flex items-center justify-between transition hover:bg-grey-light">
+                    Read More
+                    <i class="fas fa-chevron-right"></i>
+                  </div> */}
+                                </div>
+                            </div>
+                        })}
+
+
+                        {/* <div id="app" class="sm:w-1/2 mb-7  bg-white w-full md:w-1/2 rounded shadow-md flex card text-grey-darkest">
+              <img class="w-1/2 h-full rounded-l-sm" src="https://bit.ly/2EApSiC" alt="Room Image" />
+              <div class="w-full flex flex-col">
+                <div class="p-4 pb-0 flex-1">
+
+                  <span class="text-2xl text-grey-darkest">£63.00<span class="text-lg">/PPPN</span></span>
+                  <div class="flex items-center mt-4">
+                    <div class="pr-2 text-xs">
+                      <i class="fas fa-wifi text-green"></i> Free WiFi
+                    </div>
+                    <div class="px-2 text-xs">
+                      <i class="text-grey-darker far fa-building"></i> 2mins to center
+                    </div>
+                  </div>
+                </div>
+                <div
+                  class="bg-grey-lighter p-3 flex items-center justify-between transition hover:bg-grey-light">
+                  Book Now
+                  <i class="fas fa-chevron-right"></i>
+                </div>
+              </div>
+            </div>
+            <div id="app" class="sm:w-1/2 mb-7  bg-white w-full md:w-1/2 rounded shadow-md flex card text-grey-darkest">
+              <img class="w-1/2 h-full rounded-l-sm" src="https://bit.ly/2EApSiC" alt="Room Image" />
+              <div class="w-1/2 flex flex-col">
+                <div class="p-4 pb-0 flex-1">
+
+                  <span class="text-3xl text-grey-darkest">£63.00<span class="text-lg">/PPPN</span></span>
+                  <div class="flex items-center mt-4">
+                    <div class="pr-2 text-xs">
+                      <i class="fas fa-wifi text-green"></i> Free WiFi
+                    </div>
+                    <div class="px-2 text-xs">
+                      <i class="text-grey-darker far fa-building"></i> 2mins to center
+                    </div>
+                  </div>
+                </div>
+                <div
+                  class="bg-grey-lighter p-3 flex items-center justify-between transition hover:bg-grey-light">
+                  Book Now
+                  <i class="fas fa-chevron-right"></i>
+                </div>
+              </div>
+            </div>
+            <div id="app" class="sm:w-1/2 mb-7  bg-white w-full md:w-1/2 rounded shadow-md flex card text-grey-darkest">
+              <img class="w-1/2 h-full rounded-l-sm" src="https://bit.ly/2EApSiC" alt="Room Image" />
+              <div class="w-full flex flex-col">
+                <div class="p-4 pb-0 flex-1">
+
+                  <span class="text-2xl text-grey-darkest">£63.00<span class="text-lg">/PPPN</span></span>
+                  <div class="flex items-center mt-4">
+                    <div class="pr-2 text-xs">
+                      <i class="fas fa-wifi text-green"></i> Free WiFi
+                    </div>
+                    <div class="px-2 text-xs">
+                      <i class="text-grey-darker far fa-building"></i> 2mins to center
+                    </div>
+                  </div>
+                </div>
+                <div
+                  class="bg-grey-lighter p-3 flex items-center justify-between transition hover:bg-grey-light">
+                  Book Now
+                  <i class="fas fa-chevron-right"></i>
+                </div>
+              </div>
+            </div> */}
+                    </div>
+                </div>
+            </section>
 
 
             <footer class="text-gray-600 body-font bg-gray-100">
@@ -58,5 +160,13 @@ const Footer = ({ news }) => {
         </>
     )
 }
-
+export async function getServerSideProps(context) {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/footer/`)
+    const json_res = await res.json()
+    let posts = JSON.parse(JSON.stringify(json_res))
+    let news = posts.news
+    return {
+        props: { news: JSON.parse(JSON.stringify(news)) }
+    }
+}
 export default Footer
