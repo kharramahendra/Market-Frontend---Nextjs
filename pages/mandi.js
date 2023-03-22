c25a28c940b8c71cf1f1f7329cd5c0a8a9be9dad
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-03-22 12:31:10 +0530

    sharemarket views

--- pages/mandi.js
+++ pages/mandi.js
@@ -109,7 +109,7 @@ const Mandi = ({ prices }) => {
             <div className="flex flex-wrap -m-2">
 
               {prices.map((price) => {
-                return <Link key={price._id} href={`/post/${price.slug}`}><div className=" cursor-pointer p-2 lg:w-1/3 md:w-1/2 w-full">
+                return <a  href={`/post/${price.slug}`} className="cursor-pointer p-2 lg:w-1/3 md:w-1/2 w-full"><div key={price._id} className="">
                   <div className="h-full flex items-center bg-gray-800 border p-4 rounded-lg">
                     {price.image != null && <img alt="मंडी भाव राजस्थान Today" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-md mr-4" src={process.env.NEXT_PUBLIC_HOST + price.image} />}
                     {price.image == null && price.image_url != '' && <img className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-md mr-4" src={price.image_url} alt="मंडी भाव राजस्थान Today" />}
@@ -130,7 +130,7 @@ const Mandi = ({ prices }) => {
                       </div>
                     </div>
                   </div>
-                </div></Link>
+                </div></a>
               })}
 
             </div>
