7a709a700ef3524d1ef78820980f405eb72e096b
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-03-22 11:49:24 +0530

    sharemarket views

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -2,7 +2,6 @@ import React from 'react'
 import { useRouter } from 'next/router'
 
 
-
 const router = useRouter()
 
 
@@ -30,7 +29,7 @@ hljs.registerLanguage('javascript', javascript);
 
 import Head from 'next/head'
 import Image from 'next/image'
-import { useRouter } from 'next/router'
+
 import { useState } from 'react';
 import { useEffect } from 'react'
 import Navbar from '../../components/navbar'
