d9172a50345a2bf5f5074c240fc4c21f38b59a48
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-08-21 20:06:22 +0530

    sab lut gya

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ import Ticker from '@/components/ticker';
 import Script from 'next/script';
 let tvScriptLoadingPromise;
 // { news, prices }
-const Home = ({news,prices}) => {
+const Home = ({ news, prices }) => {
   // const news = []
   // const prices = []
   useEffect(() => {
@@ -392,13 +392,14 @@ const Home = ({news,prices}) => {
 
 
 export async function getServerSideProps(context) {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/home/`)
-  const json_res = await res.json()
-  let posts = JSON.parse(JSON.stringify(json_res))
-  let news = posts.news
-  let prices = posts.prices
-  console.log(prices)
-  
+  // const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/home/`)
+  // const json_res = await res.json()
+  // let posts = JSON.parse(JSON.stringify(json_res))
+  // let news = posts.news
+  // let prices = posts.prices
+  // console.log(prices)
+  const news = {}
+  const prices = {}
   return {
     props: { news: JSON.parse(JSON.stringify(news)), prices: JSON.parse(JSON.stringify(prices)) }
   }
