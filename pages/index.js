11750c576073e33319f6427273f53f9ac0b3f132
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-08-21 19:38:38 +0530

    sab lut gya

--- pages/index.js
+++ pages/index.js
@@ -240,7 +240,7 @@ const Home = ({news,prices}) => {
                   <Link href={'/mandi'}><a class="mt-3 text-indigo-500 inline-flex items-center font-semibold cursor-pointer">लाइव मंडी भाव
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                       <path d="M5 12h14M12 5l7 7-7 7"></path>
-                    </svg>
+                    </svg> 
                   </a></Link> 
                 </div>
               </div>
