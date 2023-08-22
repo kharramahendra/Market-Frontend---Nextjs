31416e1c30eae564934d31c1bf0903da9539b35a
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-08-22 20:48:26 +0530

    sab lut gya

--- pages/index.js
+++ pages/index.js
@@ -109,14 +109,14 @@ const Home = ({ news, prices }) => {
 
             {prices.map((price) => {
               return <a  href={`/post/${price.slug}`} className="cursor-pointer p-2 lg:w-1/3 md:w-1/2 w-full"><div key={price._id} className=" ">
-                <div className="h-full flex items-center bg-gray-800  border p-4 rounded-lg">
+                <div className="h-full flex items-center bg-gray-100  border-2 border-black p-4 rounded-md">
                   {price.image != null && <img alt="bhav" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-md mr-4" src={process.env.NEXT_PUBLIC_HOST + price.image} />}
                   {price.image == null && price.image_url != '' && <img className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-md mr-4" src={price.image_url} alt="tech" />}
                   {price.image == null && price.image_url == '' && <img className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-md mr-4" src='../assets/images/market.jpg' alt="tech" />}
 
                   {/* <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="" /> */}
                   <div className="flex-grow">
-                    <h2 className="text-white title-font text-lg font-bold">{price.timestamp}</h2>
+                    <h2 className="text-black title-font text-lg font-bold">{price.timestamp}</h2>
                     <div class="flex">
                         <span class="text-xs inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-pink-700 text-white rounded">{price.keywords[0]}</span>
                         {/* <span class="text-xs inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-purple-600 text-white rounded">Secondary</span> */}
