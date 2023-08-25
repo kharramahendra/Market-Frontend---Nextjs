c3f31c18e777e8d6f30571dda95b91d10913340a
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-08-25 19:45:53 +0530

    tensorcodes

--- components/footer.js
+++ components/footer.js
@@ -21,12 +21,48 @@ const Footer = () => {
     return (
         <>
 
-<a href="//www.dmca.com/Protection/Status.aspx?ID=cb7f3c03-d630-4302-9524-0da8a8eae14d" title="DMCA.com Protection Status" class="dmca-badge"> <img src ="https://images.dmca.com/Badges/dmca-badge-w200-5x1-10.png?ID=cb7f3c03-d630-4302-9524-0da8a8eae14d"  alt="DMCA.com Protection Status" /></a>  <script src="https://images.dmca.com/Badges/DMCABadgeHelper.min.js"> </script>
+            <a href="//www.dmca.com/Protection/Status.aspx?ID=cb7f3c03-d630-4302-9524-0da8a8eae14d" title="DMCA.com Protection Status" class="dmca-badge"> <img src="https://images.dmca.com/Badges/dmca-badge-w200-5x1-10.png?ID=cb7f3c03-d630-4302-9524-0da8a8eae14d" alt="DMCA.com Protection Status" /></a>  <script src="https://images.dmca.com/Badges/DMCABadgeHelper.min.js"> </script>
 
 
 
 
             <footer class="text-gray-600 body-font bg-gray-100">
+
+                {/* <!-- cards --> */}
+                <section class="container bg-white ">
+                    <div class="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16">
+                        <h1 class="text-3xl font-semibold text-center text-gray-800 capitalize lg:text-4xl ">From the blog</h1>
+
+                        <div class="grid grid-cols-1 gap-8 mt-8 md:mt-16 md:grid-cols-2">
+
+                            {news && news.map((post) => {
+                                return <div class="lg:flex">
+                                    {/* <img class="object-cover w-full h-36 rounded-lg lg:w-64"
+                                        src="https://images.unsplash.com/photo-1497032628192-86f99bcd76bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                                        alt="" /> */}
+
+                                    {post.image != null && <img class="object-cover w-full h-36 rounded-lg lg:w-64" src={process.env.NEXT_PUBLIC_HOST + post.image} alt="mandi" />}
+                                    {post.image == null && post.image_url != '' && <img class="object-cover w-full h-36 rounded-lg lg:w-64" src={post.image_url} alt="mandi" />}
+                                    {post.image == null && post.image_url == '' && <img class="object-cover w-full h-36 rounded-lg lg:w-64" src='../assets/images/news.jpg' alt="mandi" />}
+
+                                    <div class="flex flex-col justify-between py-6 lg:mx-6">
+                                        <a href="#" class="text-xl font-semibold text-gray-800 hover:underline  ">
+                                        {post.title.slice(0, 70)} ...
+                                        </a>
+
+                                        <span class="text-sm text-gray-500 dark:text-gray-700">{post.timestamp}</span>
+                                    </div>
+                                </div>
+                            })}
+
+
+                        </div>
+                    </div>
+                </section>
+                {/* <!-- cards end  --> */}
+
+
+
                 <section class="text-gray-600 body-font">
                     <div class="containerx px-5 py-16 mx-auto">
                         <div className="flex flex-col text-center w-full mb-8">
@@ -55,6 +91,8 @@ const Footer = () => {
                         </div>
                     </div>
                 </section>
+
+
 
                 <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
                     <a class="max-w-[160px] flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
