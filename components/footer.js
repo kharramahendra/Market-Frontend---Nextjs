be30f2b75229927d12a7de940a2860fa7f073276
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-10-06 16:19:41 +0530

    t&c

--- components/footer.js
+++ components/footer.js
@@ -102,7 +102,7 @@ const Footer = () => {
                 </section>
 
 <div className='md:flex'>
-<a href="/t%c" className="text-sm text-gray-800 flex hover:text-gray-900 font-bold">Terms & Conditions</a>
+<a href="/t&c" className="text-sm text-gray-800 flex hover:text-gray-900 font-bold">Terms & Conditions</a>
 <a href="/return" className="text-sm text-gray-800 flex hover:text-gray-900 font-bold">Return Policy</a>
 <a href="/ship" className="text-sm text-gray-800 flex hover:text-gray-900 font-bold">Shipping policy</a>
 <a href="/privacy.html" className="text-sm text-gray-800 flex hover:text-gray-900 font-bold">Privacy Policy</a>
